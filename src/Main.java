ae75e12315458094f928a10020887569437d19a2
Minchae0322 <jmcabc0322@gmail.com>
2023-07-20 20:44:27 +0900

    1969 DNA

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,64 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-//todo 21608 상어초등학교
+//todo 1969 DNA
 public class Main {
 
+    static class Alphabet implements Comparable<Alphabet>{
+        int alpha;
+        int cnt;
 
-    static int[] small;
-    static int sum;
+
+        public Alphabet(int alpha, int cnt) {
+            this.alpha = alpha;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Alphabet o) {
+            if (this.cnt == o.cnt) {
+                return this.alpha - o.alpha;
+            }
+            return o.cnt - this.cnt;
+        }
+    }
+
+    static Alphabet[][] alphabets;
+    static int sum = 0;
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        small = new int[9];
-        for (int i = 0; i < 9; i++) {
-            small[i] = sc.nextInt();
-            sum += small[i];
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        alphabets = new Alphabet[M][26];
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                if (sum - (small[i] + small[j]) == 100) {
-                    for (int n = 0; n < 9; n++) {
-                        if (n != i && n != j) {
-                            System.out.println(small[n]);
-                        }
-                    }
-                    return;
-                }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < 26; j++) {
+                alphabets[i][j] = new Alphabet(j + 65, 0);
             }
         }
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                char c = s.charAt(j);
+                alphabets[j][c - 65].cnt++;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            Arrays.sort(alphabets[i]);
+            sum += N - alphabets[i][0].cnt;
+            System.out.print((char) alphabets[i][0].alpha);
+        }
+        System.out.println();
+
+        System.out.println(sum);
+
+
+
 
     }
 
