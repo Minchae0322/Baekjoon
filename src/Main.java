e191219b70e48b07750d6e5e2409f99e93f387a6
Minchae0322 <jmcabc0322@gmail.com>
2023-07-24 23:05:30 +0900

    추월

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
+import java.beans.Introspector;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-//todo 9012 DNA
+//todo 추월
 public class Main {
 
     static int N;
+
+    static int result = 0;
+
+    static class Before {
+        public String front;
+
+        public boolean isOk;
+
+        public Before(String s, boolean isOk) {
+            this.front = s;
+            this.isOk = isOk;
+        }
+    }
+
+
+    static HashMap<String, Before> before = new HashMap<>();
+    static List<String> afterList = new ArrayList<>();
+    static HashMap<String, Integer> after = new HashMap<>();
 
 
 
@@ -16,35 +35,42 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        for (int t = 0; t < N; t++) {
+        String first = br.readLine();
+        before.put(first, new Before(first, true));
+        String front = first;
+        for (int i = 1; i < N; i++) {
             String s = br.readLine();
-            int num = s.length();
-            if (run(num, s)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            before.put(s, new Before(front, false));
+            front = s;
+        }
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            afterList.add(i, s);
+            after.put(s, i);
+        }
+
+
+        for (String s : afterList) {
+            String ss = s;
+            while (true) {
+                Before b = before.get(ss);
+                Before bb = before.get(b.front);
+                if (after.get(s) < after.get(b.front)) {
+                    result++;
+                    break;
+                }
+                if (bb.isOk) {
+                    b.isOk = true;
+                    break;
+                }
+                ss = b.front;
+
             }
         }
 
+        System.out.println(result);
+
     }
-
-    static boolean run(int num, String s) {
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < num; i++) {
-                char c = s.charAt(i);
-
-                if (c == '(') {
-                    stack.push('(');
-                    continue;
-                }
-
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                stack.pop();
-            }
-            return stack.isEmpty();
-        }
-    }
+}
 
