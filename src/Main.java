1b858f2f2a05e01613bad0948d85b0e370deb89e
Minchae0322 <jmcabc0322@gmail.com>
2023-07-28 19:09:06 +0900

    17142 연구소 3틀렸습니다 내일 할거

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,137 @@ import java.beans.Introspector;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.util.*;
+import java.util.function.IntPredicate;
 
 //todo 추월
 public class Main {
 
 
-    static int[] triNum;
+    static int N;
+    static int M;
+    static int[][] map;
+    static int[] yy = {0, 0, -1, 1};
+    static int[] xx = {-1, 1, 0, 0};
+
+    static List<int[]> virus = new ArrayList<>();
+
+    static boolean[] actVirus;
+
+    static int min = Integer.MAX_VALUE;
 
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int t = 0; t < test; t++) {
-            int n = sc.nextInt();
+        map = new int[N][N];
 
-            System.out.println(init(n));
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    virus.add(new int[]{i, j});
+                }
+            }
+        }
+        actVirus = new boolean[virus.size()];
+        backTracking(0, 0);
+
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(min);
+
+    }
+
+    static void backTracking(int depth, int start) {
+        if (depth == M) {
+            List<int[]> list = new ArrayList<>();
+            for (int i = 0; i < virus.size(); i++) {
+                if (actVirus[i]) {
+                    list.add(virus.get(i));
+                }
+            }
+            if (list.size() >= M) {
+                min = Math.min(bfs(list), min);
+            }
+            return;
+        }
+
+        for (int i = start; i < virus.size(); i++) {
+            int[] v = virus.get(i);
+            actVirus[i] = true;
+            backTracking(depth + 1, start + 1);
+            actVirus[i] = false;
+        }
+    }
+
+    static int bfs(List<int[]> startVirus) {
+        int[][] board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(map[i], 0, board[i], 0, N);
+        }
+        Queue<int[]> que = new LinkedList<>();
+        for (int[] v : startVirus) {
+            board[v[0]][v[1]] = 3;
+            que.add(new int[]{v[0], v[1]});
+        }
+
+
+        int max = 3;
+        while (!que.isEmpty()) {
+            int[] now = que.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int dy = now[0] + yy[i];
+                int dx = now[1] + xx[i];
+
+                if (dy < 0 || dx < 0 || dy >= N || dx >= N) {
+                    continue;
+                }
+
+                if (board[dy][dx] == 1) {
+                    continue;
+                }
+
+                if (board[dy][dx] == 0) {
+                    board[dy][dx] = board[now[0]][now[1]] + 1;
+                    max = board[dy][dx];
+                    que.add(new int[]{dy, dx});
+                }
+
+                if (board[dy][dx] == 2) {
+                    board[dy][dx] = board[now[0]][now[1]];
+                    que.add(new int[]{dy, dx});
+                }
+            }
         }
 
 
 
-
-
-
-    }
-
-    static int init(int n) {
-        for (int i = 1; i < 100; i++) {
-            int check1 = (i*(i+1)) / 2;
-            if (check1 > 1000) {
-                break;
-            }
-            for (int j = 1; j < 100; j++) {
-                int check2 = (i*(i+1)) / 2;
-                if (check2 > 1000) {
-                    break;
-                }
-                for (int k = 1; k < 100; k++) {
-                    int check3 = (i*(i+1)) / 2;
-                    if (check3 > 1000) {
-                        break;
-                    }
-                    int tri = ((i * (i + 1)) / 2) + ((j * (j + 1)) / 2) + ((k * (k + 1)) / 2);
-                    if (tri == n) {
-                        return 1;
-                    }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 0) {
+                    return Integer.MAX_VALUE;
                 }
             }
         }
-        return 0;
+        return max - 3;
     }
+
+
+
+
+
+
+
 }
 
