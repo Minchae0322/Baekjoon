70ae6df6197b0aeb7817c9cd4498a1a2ed061267
Minchae0322 <jmcabc0322@gmail.com>
2023-07-30 20:54:31 +0900

    숫자야구

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,17 @@ public class Main {
 
 
     static int N;
-    static int M;
-    static int[][] map;
-    static int[] yy = {0, 0, -1, 1};
-    static int[] xx = {-1, 1, 0, 0};
+    static List<Num> list = new ArrayList<>();
 
-    static List<Virus> virus = new ArrayList<>();
+    static class Num {
+        public char[] num;
+        public int strike;
+        public int ball;
 
-    static boolean[] actVirus;
-
-    static int min = Integer.MAX_VALUE;
-
-    static int zero =0;
-
-    static class Virus {
-        int y;
-        int x;
-        int time;
-
-        public Virus(int y, int x, int time) {
-            this.y = y;
-            this.x = x;
-            this.time = time;
+        public Num(char[] num, int strike, int ball) {
+            this.num = num;
+            this.strike = strike;
+            this.ball = ball;
         }
     }
 
@@ -42,101 +31,65 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][N];
-
+        int result = 0;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) {
-                    virus.add(new Virus(i, j, 0));
-                    continue;
-                }
-                if (map[i][j] == 0) {
-                    zero++;
-                }
+            String num = st.nextToken();
+            list.add(new Num(new char[]{num.charAt(0), num.charAt(1), num.charAt(2)}, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        for (int i = 111; i <= 999; i++) {
+            String str = String.valueOf(i);
+            if(str.charAt(0)=='0' || str.charAt(1)=='0' || str.charAt(2)=='0') continue;
+
+            // 문제에 따르면 모든 자릿수의 숫자가 달라야 하기 때문에 같은 숫자가 있을 경우 제외
+            if(str.charAt(0)==str.charAt(1) || str.charAt(0)==str.charAt(2) || str.charAt(1)==str.charAt(2)) continue;
+            if (run(i)) {
+                result++;
             }
         }
-        actVirus = new boolean[virus.size()];
-        backTracking(0, 0);
 
-        if (min == Integer.MAX_VALUE) {
-            System.out.println(-1);
-            return;
-        }
-
-        System.out.println(min);
-
-    }
-
-    static void backTracking(int depth, int start) {
-        if (depth == M) {
-            min = Math.min(bfs(), min);
-        } else {
-            for (int i = start; i < virus.size(); i++) {
-                if (!actVirus[i]) {
-                    actVirus[i] = true;
-                    backTracking(depth + 1, i + 1);
-                    actVirus[i] = false;
-
-                }
-
-
-            }
-        }
+        System.out.println(result);
 
 
     }
 
-    static int bfs() {
-
-        Queue<Virus> que = new LinkedList<>();
-        boolean[][] isChecked = new boolean[N][N];
-        for (int i = 0; i < virus.size(); i++) {
-            if (actVirus[i]) {
-                Virus v = virus.get(i);
-                que.add(new Virus(v.y, v.x, 0));
+    static boolean run(int expectedNum) {
+        String ex = String.valueOf(expectedNum);
+        for (Num now : list) {
+            // System.out.println(ex +  " strike: " + getStrike(ex, now));
+            int strike = getStrike(ex, now);
+            if (now.strike != strike) {
+                return false;
+            }
+            //System.out.println(ex +  " ball: " + getBall(ex, now));
+            if (now.ball != getBall(ex, now) - strike) {
+                return false;
             }
         }
+        return true;
+    }
 
-        int max = 0;
-        int count = 0;
-        while (!que.isEmpty()) {
-            Virus now = que.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int dy = now.y + yy[i];
-                int dx = now.x + xx[i];
-
-                if (dy < 0 || dx < 0 || dy >= N || dx >= N) {
-                    continue;
-                }
-
-                if (map[dy][dx] == 1 || isChecked[dy][dx]) {
-                    continue;
-                }
-
-                if (!isChecked[dy][dx] && map[dy][dx] != 1) {
-                    if (map[dy][dx] == 0) {
-                        count++;
-                        max = now.time + 1;
-                    }
-                    isChecked[dy][dx] = true;
-                    que.add(new Virus(dy, dx, now.time + 1));
-                }
-
+    static int getStrike(String ex, Num now) {
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (ex.charAt(i) == now.num[i]) {
+                strike++;
             }
         }
+        return strike;
+    }
 
-        if (zero == count) {
-            return max;
+
+    static int getBall(String ex, Num now) {
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (ex.contains(String.valueOf(now.num[i]))) {
+                ball++;
+            }
         }
-
-
-        return Integer.MAX_VALUE;
+        return ball;
     }
 
 
