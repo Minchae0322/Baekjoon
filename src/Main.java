d4a2813becdaa6ef297c7e4957b4ba1d6ce595d3
Minchae0322 <jmcabc0322@gmail.com>
2023-08-01 00:37:16 +0900

    폴리오미노

--- src/Main.java
+++ src/Main.java
@@ -6,91 +6,47 @@ import java.net.Inet4Address;
 import java.util.*;
 import java.util.function.IntPredicate;
 
-//todo 추월
+//todo 1343 폴리오미노
 public class Main {
 
 
-    static int N;
-    static List<Num> list = new ArrayList<>();
 
-    static class Num {
-        public char[] num;
-        public int strike;
-        public int ball;
-
-        public Num(char[] num, int strike, int ball) {
-            this.num = num;
-            this.strike = strike;
-            this.ball = ball;
-        }
-    }
 
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        int result = 0;
+        String s = br.readLine();
+        StringBuilder result = new StringBuilder();
+        String[] ss = s.split("\\.");
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String num = st.nextToken();
-            list.add(new Num(new char[]{num.charAt(0), num.charAt(1), num.charAt(2)}, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
-
-        for (int i = 111; i <= 999; i++) {
-            String str = String.valueOf(i);
-            if(str.charAt(0)=='0' || str.charAt(1)=='0' || str.charAt(2)=='0') continue;
-
-            // 문제에 따르면 모든 자릿수의 숫자가 달라야 하기 때문에 같은 숫자가 있을 경우 제외
-            if(str.charAt(0)==str.charAt(1) || str.charAt(0)==str.charAt(2) || str.charAt(1)==str.charAt(2)) continue;
-            if (run(i)) {
-                result++;
+        for (int i = 0; i < ss.length; i++) {
+            if (ss[i].length() % 2 == 1) {
+                System.out.println(-1);
+                return;
             }
+            if (ss[i].length() == 4) {
+                result.append("AAAA");
+            } else {
+                result.append("BB");
+            }
+            result.append(".");
         }
 
-        System.out.println(result);
+        if (s.charAt(0) == '.') {
+            System.out.println("." + result);
+        } else {
+            System.out.println(result);
+        }
+
+
+
+
 
 
     }
 
-    static boolean run(int expectedNum) {
-        String ex = String.valueOf(expectedNum);
-        for (Num now : list) {
-            // System.out.println(ex +  " strike: " + getStrike(ex, now));
-            int strike = getStrike(ex, now);
-            if (now.strike != strike) {
-                return false;
-            }
-            //System.out.println(ex +  " ball: " + getBall(ex, now));
-            if (now.ball != getBall(ex, now) - strike) {
-                return false;
-            }
-        }
-        return true;
-    }
 
-    static int getStrike(String ex, Num now) {
-        int strike = 0;
-        for (int i = 0; i < 3; i++) {
-            if (ex.charAt(i) == now.num[i]) {
-                strike++;
-            }
-        }
-        return strike;
-    }
-
-
-    static int getBall(String ex, Num now) {
-        int ball = 0;
-        for (int i = 0; i < 3; i++) {
-            if (ex.contains(String.valueOf(now.num[i]))) {
-                ball++;
-            }
-        }
-        return ball;
-    }
 
 
 
