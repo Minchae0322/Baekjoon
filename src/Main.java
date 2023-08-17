98f3e593156c250e68018b0e34afab3c50dd49b2
Minchae0322 <jmcabc0322@gmail.com>
2023-08-17 19:44:26 +0900

    지구온난화

--- src/Main.java
+++ src/Main.java
@@ -2,68 +2,102 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.jar.JarEntry;
 
-//todo 기타 레슨 2343
+//todo 지구온난화
 public class Main {
 
-    static int N;
-    static int M;
+    static int R;
+    static int C;
 
-    static int[] value;
+    static char[][] value;
 
+    static int[] yy = {0, 0, -1, 1};
+    static int[] xx = {-1, 1, 0, 0};
 
+    static Queue<int[]> que = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        value = new int[N];
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int min = 0;
-        int max = 0;
+        value = new char[R][C];
 
-        for (int i = 0; i < N; i++) {
-            value[i] = Integer.parseInt(st.nextToken());
-            min = Math.max(min, value[i]);
-            max += value[i];
-        }
+        for (int i = 0; i < R; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < C; j++) {
+                value[i][j] = s.charAt(j);
+                if (value[i][j] == 'X') {
+                    que.add(new int[]{i, j});
 
-        int start = min;
-        int end = max;
-
-        while (start <= end) {
-            int middle = (start + end) / 2;
-            int blueRay = divide(middle);
-
-            if (blueRay <= M) {
-                end = middle - 1;
-            } else {
-                start = middle + 1;
+                }
             }
         }
 
-        System.out.println(start);
-
+        getMap();
 
     }
 
-    static int divide(int num) {
-        int blueRayNum = 0;
-        int blueRayVol = 0;
-        for (int i = 0; i < N; i++) {
-            if (blueRayVol + value[i] > num) {
-                blueRayNum++;
-                blueRayVol = 0;
+    static void getMap() {
+        char[][] map = fifty();
+        int startY = R;
+        int startX = C;
+        int endY = 0;
+        int endX = 0;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+
+                if (map[i][j] == 'X') {
+                    startY = Math.min(startY, i);
+                    startX = Math.min(startX, j);
+                    endY = Math.max(endY, i);
+                    endX = Math.max(endX, j);
+
+                }
             }
-            blueRayVol += value[i];
+        }
 
+        for (int i = startY; i <= endY; i++) {
+            for (int j = startX; j <= endX; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
         }
-        if (blueRayVol == 0) {
-            return blueRayNum;
-        }
-        return blueRayNum + 1;
     }
+
+
+    static char[][] fifty() {
+        char[][] v = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                v[i][j] = value[i][j];
+            }
+        }
+
+        while (!que.isEmpty()) {
+            int[] now = que.poll();
+            int cnt = 0;
+            for (int i = 0; i < 4; i++) {
+                int dy = now[0] + yy[i];
+                int dx = now[1] + xx[i];
+
+                if (dy < 0 || dx < 0 || dy > R - 1 || dx > C - 1) {
+                    cnt++;
+                    continue;
+                }
+                if (value[dy][dx] == '.') {
+                    cnt++;
+                }
+            }
+            if (cnt >= 3) {
+                v[now[0]][now[1]] = '.';
+            }
+        }
+        return v;
+    }
+
 
 
 
