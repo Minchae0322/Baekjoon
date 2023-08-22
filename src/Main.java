46465f0bc391499e21136b3a7de5adc1c3b937b3
Minchae0322 <jmcabc0322@gmail.com>
2023-08-22 17:54:24 +0900

    지구온난화

--- src/Main.java
+++ src/Main.java
@@ -7,96 +7,102 @@ import java.util.jar.JarEntry;
 //todo 지구온난화
 public class Main {
 
+    static int[][] map;
     static int R;
     static int C;
 
-    static char[][] value;
+    static int N;
 
     static int[] yy = {0, 0, -1, 1};
     static int[] xx = {-1, 1, 0, 0};
 
-    static Queue<int[]> que = new LinkedList<>();
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        value = new char[R][C];
+
+
+
+
+    }
+
+    static void down(boolean[][] isChecked) {
+        List<int[]> list = new ArrayList<>();
 
         for (int i = 0; i < R; i++) {
-            String s = br.readLine();
             for (int j = 0; j < C; j++) {
-                value[i][j] = s.charAt(j);
-                if (value[i][j] == 'X') {
-                    que.add(new int[]{i, j});
-
+                if (map[i][j] == 'x' && !isChecked[i][j]) {
+                    list.add(new int[]{i, j});
                 }
             }
         }
 
-        getMap();
-
-    }
-
-    static void getMap() {
-        char[][] map = fifty();
-        int startY = R;
-        int startX = C;
-        int endY = 0;
-        int endX = 0;
-
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-
-                if (map[i][j] == 'X') {
-                    startY = Math.min(startY, i);
-                    startX = Math.min(startX, j);
-                    endY = Math.max(endY, i);
-                    endX = Math.max(endX, j);
-
-                }
-            }
-        }
-
-        for (int i = startY; i <= endY; i++) {
-            for (int j = startX; j <= endX; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
+        if (isAbleDown(list, isChecked)) {
+            move(isChecked, list);
+            down(isChecked);
         }
     }
 
+    static void move(boolean[][] isChecked, List<int[]> list) {
+        for (int[] now : list) {
+            int dy = now[0] + 1;
+            int dx = now[1];
 
-    static char[][] fifty() {
-        char[][] v = new char[R][C];
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                v[i][j] = value[i][j];
+            map[dy][dx] = 'x';
+            map[now[0]][now[1]] = '.';
+            isChecked[dy][dx] = false;
+            isChecked[now[0]][now[1]] = true;
+        }
+    }
+
+    static boolean isAbleDown(List<int[]> list, boolean[][] isChecked) {
+        for (int[] now : list) {
+            int dy = now[0] + 1;
+            int dx = now[1];
+            if (dy < 0) {
+                return false;
+            }
+            if (map[dy][dx] == 'x' && isChecked[dy][dx]) {
+                return false;
             }
         }
+        return true;
+    }
+
+    static void bfs(boolean[][] isChecked, int y, int x) {
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[]{y, x});
 
         while (!que.isEmpty()) {
             int[] now = que.poll();
-            int cnt = 0;
             for (int i = 0; i < 4; i++) {
                 int dy = now[0] + yy[i];
                 int dx = now[1] + xx[i];
 
                 if (dy < 0 || dx < 0 || dy > R - 1 || dx > C - 1) {
-                    cnt++;
                     continue;
                 }
-                if (value[dy][dx] == '.') {
-                    cnt++;
+
+                if (!isChecked[dy][dx] && map[dy][dx] == 'x') {
+                    que.add(new int[]{dy, dx});
+                    isChecked[dy][dx] = true;
                 }
-            }
-            if (cnt >= 3) {
-                v[now[0]][now[1]] = '.';
+
             }
         }
-        return v;
+        down(isChecked);
     }
+
+
+
+
+
+
+
 
 
 
