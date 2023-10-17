ea13c51c8d290ea01f5022d6674c2d7f19a0378c
Minchae0322 <jmcabc0322@gmail.com>
2023-10-17 15:12:06 +0900

    프로그래머스  요격시스템

--- src/Main.java
+++ src/Main.java
@@ -23,232 +23,46 @@ public class Main {
     static int block = 0;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        Scanner sc = new Scanner(System.in);
+        map = new int[7][2];
 
-        board = new char[R][C];
-
-        for (int i = 0; i < R; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < C; j++) {
-                char c = s.charAt(j);
-                if (c == 'M') {
-                    M = new int[]{i, j};
-                }
-                if (c == 'Z') {
-                    Z = new int[]{i, j};
-                }
-                if (c != 'M' && c != '.') {
-                    if (c == '+') {
-                        block += 2;
-                    } else {
-                        block++;
-                    }
-
-                }
-                board[i][j] = c;
-            }
+        for (int i = 0; i < 7; i++) {
+            map[i][0] = sc.nextInt();
+            map[i][1] = sc.nextInt();
         }
 
-
-        run();
-
-    }
-
-    static void run() {
-        int dy = M[0];
-        int dx = M[1];
-        int dir = startDirection();
-
-        while (canFlow(dy, dx, dir)) {
-            dy = dy + yy[dir];
-            dx = dx + xx[dir];
-            dir = getDir(dy, dx, dir);
-        }
-        makeBlock(dy, dx, dir);
-    }
-
-    static boolean run2(int blockNum) {
-        int dy = M[0];
-        int dx = M[1];
-        int dir = startDirection();
-
-        while (canFlow(dy, dx, dir)) {
-            dy = dy + yy[dir];
-            dx = dx + xx[dir];
-            dir = getDir(dy, dx, dir);
-            blockNum--;
-            if ((dy == Z[0] && dx == Z[1])) {
-                break;
-
-            }
-
-        };
-        return blockNum == 0;
-    }
-
-    static void makeBlock(int y, int x, int dir) {
-        int blockY = y + yy[dir];
-        int blockX = x + xx[dir];
-
-
-        board[blockY][blockX] = '|';
-        if (run2(block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '|');
-            return;
-        }
-        board[blockY][blockX] = '-';
-        if (run2( block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '-');
-            return;
-        }
-        board[blockY][blockX] = '+';
-        if (run2(block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '+');
-            return;
-        }
-        board[blockY][blockX] = '1';
-        if (run2(block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '1');
-            return;
-        }
-        board[blockY][blockX] = '2';
-        if (run2(block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '2');
-            return;
-        }board[blockY][blockX] = '3';
-        if (run2(block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '3');
-            return;
-        }board[blockY][blockX] = '4';
-        if (run2(block + 1)) {
-            blockY++;
-            blockX++;
-            System.out.println(blockY + " " + blockX + " " + '4');
-            return;
-        }
-
-
+        System.out.println(solution(map));
 
 
     }
 
-
-
-    static int getDir(int y, int x, int dir) {
-        switch (board[y][x]) {
-            case '-':
-            case '|':
-            case '+':
-                return dir;
-            case '1':
-                if (dir == 0) {
-                    return 3;
-                } else {
-                    return 1;
-                }
-
-            case '2':
-                if (dir == 3) {
-                    return 1;
-                } else {
-                    return 2;
-                }
-
-            case '3':
-                if (dir == 3) {
-                    return 0;
-                } else {
-                    return 2;
-                }
-            case '4':
-                if (dir == 1) {
-                    return 3;
-                } else {
-                    return 0;
-                }
+    static int solution(int[][] targets) {
+        int answer = 0;
+        sort(targets);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(targets[i][0] + " " + targets[i][1]);
         }
-
-        return dir;
+        answer = run(targets);
+        return answer;
     }
 
+    static void sort(int[][] targets) {
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
+    }
 
-    static int startDirection() {
-        for (int i = 0; i < 4; i++) {
-            int dy = M[0] + yy[i];
-            int dx = M[1] + xx[i];
-
-            if (dy < 0 || dx < 0 || dy >= R || dx >= C) {
+    static int run(int[][] sorted) {
+        int num = 0;
+        int firstNum = sorted[0][1];
+        for(int i=1; i<sorted.length; i++) {
+            if(firstNum < sorted[i][0]) {
                 continue;
             }
-
-            if (board[dy][dx] != '.' && board[dy][dx] != 'Z') {
-                return i;
-            }
+            num++;
+            firstNum = sorted[i][1];
         }
-        return 0;
+
+        return num;
     }
-
-    static boolean canFlow(int y, int x, int direction) {
-        int dy = y + yy[direction];
-        int dx = x + xx[direction];
-
-        if (dy < 0 || dx < 0 || dy >= R || dx >= C) {
-            return false;
-        }
-
-        if (board[dy][dx] == '.' || board[dy][dx] == 'M') {
-            return false;
-        }
-
-        if (board[dy][dx] == '|') {
-            return direction != 0 && direction != 1;
-        }
-
-        if (board[dy][dx] == '-') {
-            return direction != 2 && direction != 3;
-        }
-
-        if (board[dy][dx] == '1') {
-            return direction != 1 && direction != 3;
-        }
-
-        if (board[dy][dx] == '2') {
-            return direction != 1 && direction != 2;
-        }
-
-        if (board[dy][dx] == '3') {
-            return direction != 0 && direction != 2;
-        }
-
-        if (board[dy][dx] == '4') {
-            return direction != 0 && direction != 3;
-        }
-
-        return true;
-    }
-
-
-
-
-
-
-
-
 
 }
 
