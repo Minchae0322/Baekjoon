3605cce75662b7facf59af6e46401814de03ee34
Minchae0322 <jmcabc0322@gmail.com>
2023-10-23 16:54:25 +0900

    프로그래머스 연속된 부분 수열의 합

--- src/Main.java
+++ src/Main.java
@@ -1,101 +1,47 @@
 import java.util.*;
 
-class Main {
-    static List<Section> sections = new ArrayList<>();
+class Solution {
 
+    static class Store {
+        int start;
+        int end;
+        int length;
 
-    static class Section implements Comparator<Section>{
-        int section;
-        int stoneValue;
-        List<String> mineralsList;
-
-        public Section(int section, int stoneValue, List<String> mineralsList) {
-            this.section = section;
-            this.stoneValue = stoneValue;
-            this.mineralsList = mineralsList;
+        public Store(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.length = end - start;
         }
-
-        @Override
-        public int compare(Section o1, Section o2) {
-            return o2.stoneValue - o1.stoneValue;
-        }
-
     }
 
-    public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        int allSection = minerals.length / 5 + 1;
-        int picksNum = 0;
+    static int dp[];
+    static List<Store> list = new ArrayList<>();
 
-        picksNum += picks[0] + picks[1] + picks[2];
-        int findSection = allSection > picksNum ? picksNum : allSection;
-
-        for(int section = 0; section < findSection; section++) {
-            List<String> oneSection = new ArrayList<>();
-            for(int i=0; i<5; i++) {
-                int index = section * 5 + i;
-                if(section * 5 + i >= minerals.length) {
-                    break;
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = {};
+        dp = new int[sequence.length];
+        for(int i=0; i<sequence.length; i++) {
+            for(int j=0; j<=i; j++) {
+                dp[j] = dp[j] + sequence[i];
+                if(dp[j] == k) {
+                    list.add(new Store(j, i));
                 }
-                oneSection.add(minerals[index]);
-            }
-            int stoneValue = stonePick(oneSection);
-            sections.add(new Section(section, stoneValue, oneSection));
-        }
-        Collections.sort(sections, (a, b) -> b.stoneValue - a.stoneValue);
-
-        for(Section s : sections) {
-            if(picks[0] > 0) {
-                picks[0]--;
-                answer += diamondPick(s.mineralsList);
-                continue;
-            }
-            if(picks[1] > 0) {
-                picks[1]--;
-                answer += ironPick(s.mineralsList);
-                continue;
-            }
-            if(picks[2] > 0) {
-                picks[2]--;
-                answer += s.stoneValue;
             }
         }
 
+        Collections.sort(list, (o1, o2) -> {
+            if(o1.length == o2.length) {
+                return o1.start - o2.start;
+            }
+            return o1.length - o2.length;
+        });
+
+        Store s = list.get(0);
+        int index = 0;
+        answer = new int[2];
+        answer[0] = s.start;
+        answer[1] = s.end;
 
         return answer;
-    }
-
-
-
-    static int diamondPick(List<String> minerals) {
-        return minerals.size();
-    }
-
-    static int ironPick(List<String> minerals) {
-        int sum = 0;
-        for(String mineral : minerals) {
-            if(mineral.equals("diamond")) {
-                sum += 5;
-                continue;
-            }
-            sum += 1;
-        }
-        return sum;
-    }
-
-    static int stonePick(List<String> minerals) {
-        int sum = 0;
-        for(String mineral : minerals) {
-            if(mineral.equals("diamond")) {
-                sum += 25;
-                continue;
-            }
-            if(mineral.equals("iron")) {
-                sum += 5;
-                continue;
-            }
-            sum += 1;
-        }
-        return sum;
     }
 }
