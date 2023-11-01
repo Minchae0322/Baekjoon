c3da21f016c1bb593bf776e6948f7e0fb0d33783
Minchae0322 <jmcabc0322@gmail.com>
2023-11-01 15:59:22 +0900

    프로그래머스: 대충만든 자판

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static int[] sequencePlus;
-    static int[] sequenceMinus;
-    static int sequenceLength;
-    static long max = -100001;
+    static int[] alphabat = new int[26];
+    static int[] result;
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = {};
+        result = new int[targets.length];
+        Arrays.fill(alphabat, Integer.MAX_VALUE);
+        makeNumber(keymap);
+        return solve(targets);
 
-    public long solution(int[] sequence) {
-        sequenceLength = sequence.length;
-        sequencePlus = new int[sequenceLength];
-        sequenceMinus = new int[sequenceLength];
-        getSequencePulse(sequence);
-        getMaxSum(sequencePlus);
-        getMaxSum(sequenceMinus);
-        return max;
+
     }
 
-    static void getSequencePulse(int[] sequence) {
-        for(int i=0; i<sequenceLength; i++) {
-            if(i % 2 == 0) {
-                sequencePlus[i] = sequence[i];
-                sequenceMinus[i] = -sequence[i];
-            } else {
-                sequenceMinus[i] = sequence[i];
-                sequencePlus[i] = -sequence[i];
+    static int[] solve(String[] targets) {
+        for(int i=0; i<targets.length; i++) {
+            int sum = 0;
+            String s = targets[i];
+            for(int j=0; j<s.length(); j++) {
+                char c = s.charAt(j);
+                if(alphabat[c - 65] == Integer.MAX_VALUE) {
+                    result[i] = -1;
+                    break;
+                }
+                sum += alphabat[c - 65];
             }
+            result[i] = sum;
         }
+        return result;
     }
 
-    static void getMaxSum(int[] sequence) {
-        long sum = 0;
-        for(int i=0; i<sequenceLength; i++) {
-            sum += sequence[i];
-            max = Math.max(max, sum);
-            if(sum < 0) {
-                sum = 0;
+    static void makeNumber(String[] keymap) {
+        for(int i=0; i<keymap.length; i++) {
+            String s = keymap[i];
+            for(int j=0; j<s.length(); j++) {
+                char c = s.charAt(j);
+                alphabat[c - 65] = Math.min(j + 1, alphabat[c - 65]);
             }
         }
     }
